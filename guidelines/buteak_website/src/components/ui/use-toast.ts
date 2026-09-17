6b4087fefe9bb1d20188387e3d97bb6ae839f3a2
pankajverma2108 <pv14720@gmail.com>
2026-09-17 17:41:35 +0530

    chore: remove and ignore docs, guidelines, envs, and dot files

--- guidelines/buteak_website/src/components/ui/use-toast.ts
+++
@@ -1,5 +0,0 @@
-
-// Re-export from the correct location
-import { useToast, toast } from "@/hooks/use-toast";
-
-export { useToast, toast };
--- guidelines/buteak_website/src/pages/NewComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NewComponent() {
-    return (
-        <div className='text-[32px]  heading'>NewComponent</div>
-    )
-}
-
-export default NewComponent
